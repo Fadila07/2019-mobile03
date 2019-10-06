df55cc6be5daef0b603b9a614017024e977de641
Fadila07 <rfadila195@gmail.com>
2019-10-06 18:37:50 +0700

    membuat menu login dan help

--- TiketKereta/app/src/main/java/id/ac/polinema/tiketkereta/tampilan/welcome.java
+++ TiketKereta/app/src/main/java/id/ac/polinema/tiketkereta/tampilan/welcome.java
@@ -2,7 +2,9 @@ package id.ac.polinema.tiketkereta.tampilan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import id.ac.polinema.tiketkereta.R;
 
@@ -12,5 +14,15 @@ public class welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void clicklogin(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public void clickhelp(View view) {
+        Intent intent = new Intent(this, Help.class);
+        startActivity(intent);
     }
 }
